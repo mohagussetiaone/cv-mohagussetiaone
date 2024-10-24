d8bebe52c72e973aa3c51275a8e1786444cb4b03
mohagussetiaone <mohagussetiaone@gmail.com>
2024-10-24 20:55:16 +0700

    adding, meta tags, robot txt, using SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="rfHxt49m6Pm8OYRF_sbphjX7fCLLlfY_RibGFeNQuzs" />
+        <title>Moh Agus Setiaone - Frontend Developer | React Developer</title>
+        <meta
+          name="description"
+          content="Moh Agus Setiaone adalah Frontend Developer berpengalaman yang mahir dalam React.js dan bekerja di Remala Abadi. Spesialisasi dalam membangun aplikasi web interaktif dan performa tinggi menggunakan teknologi modern seperti Next.js, Tailwind CSS, dan React. Jelajahi portofolio untuk melihat proyek dan pengalaman terkini."
+        />
+        <meta name="keywords" content="Moh Agus Setiaone, Frontend Developer, React Developer, Remala Abadi, Next.js, Tailwind CSS, pengembangan web, portofolio, aplikasi web" />
+        <meta name="author" content="Moh Agus Setiaone" />
+        <meta name="robots" content="index, follow" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-6xl mx-auto`}>
         <Navbar />
